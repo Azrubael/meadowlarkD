c623f08b8c3b4b6c33a291d4b15895b79ab15d25
Azrubael <i0638464000@gmail.com>
2022-10-30 12:28:38 +0200

    resolved conflicts with require('express-handlebars')

--- meadowlark_index.js
+++ meadowlark_index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const ehbs = require('express-handlebars')
 const fortune = require('./lib/fortune.js')
+
 const app = express()
-const port = process.env.PORT || 9001
+
 // const handlers = require('./lib/handlers.js')
 
 // Настройка механизма представлений Handlebars
-
 const hbs = ehbs.create({defaultLayout: 'main'})
 app.engine('handlebars', hbs.engine) 
 app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 9001
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => res.render('home'))
@@ -40,6 +43,7 @@ app.use((err, req, res, next) => {
 // for testing
 // app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
+app.listen(port, () => { console.log(
     `Express running on http://localhost:${port}; ` +
-    `press Ctrl+C to stop` ))
+    `; press Ctrl+C to stop` )}
+    )
